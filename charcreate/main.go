2a27b3665c4c970e2237e63738aff8cd867b9073
toferc <cgeist7@gmail.com>
2018-09-15 21:57:13 -0400

    add skills, modifiers, abilities

--- charcreate/main.go
+++ charcreate/main.go
@@ -8,9 +8,45 @@ import (
 
 func main() {
 	c := runequest.Character{
-		Name:       "Bigby",
-		Statistics: runequest.RuneQuestStats,
+		Name:            "Bigby",
+		Statistics:      runequest.RuneQuestStats,
+		Abilities:       runequest.Abilities,
+		Skills:          runequest.Skills,
+		SkillCategories: runequest.SkillCategories,
 	}
 
+	fmt.Println(c)
+
+	m := runequest.Modifier{
+		Object: c.Statistics["STR"],
+		Value:  3,
+	}
+
+	m.ModifyValue()
+
+	mAbility := runequest.Modifier{
+		Object: c.Abilities["Air"],
+		Value:  60,
+	}
+
+	mAbility.ModifyValue()
+
+	c.DetermineSkillCategoryValues()
+
+	mSkill := runequest.Modifier{
+		Object: c.Skills["Broadsword"],
+		Value:  25,
+	}
+
+	mSkill.ModifyValue()
+
+	mPassion := runequest.Modifier{
+		Object: c.Abilities["Loyalty (Clan)"],
+		Value:  10,
+	}
+
+	mPassion.ModifyValue()
+
+	fmt.Println("")
 	fmt.Println(c)
 }
