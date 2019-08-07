92c0c5a043a2f84279feacb2f6d0469d80ba17fa
toferc <cgeist7@gmail.com>
2019-08-06 23:19:25 -0400

    update character string

--- character.go
+++ character.go
@@ -135,6 +135,10 @@ func (c Character) String() string {
 		text += fmt.Sprintf("\nHomeland: %s", c.Homeland.Name)
 		text += fmt.Sprintf("\nOccupation: %s", c.Occupation.Name)
 		text += fmt.Sprintf("\n%s of Cult: %s", c.Cult.Rank, c.Cult.Name)
+
+		text += fmt.Sprintf("\nStandard of Living: %s", c.StandardofLiving)
+		text += fmt.Sprintf("\nIncome: %d L", c.Income)
+		text += fmt.Sprintf("\nRansom: %d L", c.Ransom)
 	}
 
 	text += fmt.Sprintf("\n\nDescription:\n%s", c.Description)
